a3c2d1f02844a44499ce4f37ec2a8556dee52d04
Rohit <kumarsinghrohit585@gmail.com>
2024-07-27 10:59:59 +0530

    pusing dawn theme

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.liquid", "./src/**/*.html", "./src/**/*.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
